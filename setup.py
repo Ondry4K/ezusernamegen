e567841fda36f8af427c9da9b168f493447d162a
Ondry <45491037+Ondry4K@users.noreply.github.com>
2024-08-25 14:49:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.platform != 'win32':
 
 setup(
     name='ezusergen',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple username generator that combines random words with numbers',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
